19e6673c666fa20981a34ef5bef97c2c264362f1
elin <lyuda_shm@mail.ru>
2020-04-20 02:46:58 +0300

    Remove files in .sh

--- nfdump_to_data.sh
+++ nfdump_to_data.sh
@@ -1,4 +1,5 @@
 echo "Making input files"
+rm forplot foo.png
 nfdump -r nfcapd.202002251200 > data
 echo -e "ip: 217.15.20.194\nk: 1\nfirstfree: 1000" > tarif
 echo -e "Installing dependencies for plotting"
