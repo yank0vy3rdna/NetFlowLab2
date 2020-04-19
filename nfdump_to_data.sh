0301a3fb23ad826128004c124572fb57ef5b4f15
elin <lyuda_shm@mail.ru>
2020-04-20 02:40:09 +0300

    .sh

--- nfdump_to_data.sh
+++ nfdump_to_data.sh
@@ -3,6 +3,6 @@ nfdump -r nfcapd.202002251200 > data
 echo -e "ip: 217.15.20.194\nk: 1\nfirstfree: 1000" > tarif
 echo -e "Installing dependencies for plotting"
 echo "Compiling"
-g++ -o lab2.out -I/usr/include/python2.7 -std=c++11 -lpython2.7 main.cpp
+g++ -o lab2.out main.cpp
 echo "Computing"
 ./lab2.out
